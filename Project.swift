730b917b51c0cff35b073ffd14ffaed15671df1d
Pawel Kacela <pawel.kacela@gmail.com>
2026-03-09 20:12:11 +0100

    initial, project created with tuist

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "TuistTCAExample",
+    targets: [
+        .target(
+            name: "TuistTCAExample",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.TuistTCAExample",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["TuistTCAExample/Sources/**"],
+            resources: ["TuistTCAExample/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "TuistTCAExampleTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.TuistTCAExampleTests",
+            infoPlist: .default,
+            sources: ["TuistTCAExample/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TuistTCAExample")]
+        ),
+    ]
+)
---
+++ TuistTCAExample/Sources/TuistTCAExampleApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct TuistTCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
