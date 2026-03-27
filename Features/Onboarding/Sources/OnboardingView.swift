8a58a83685c0fc11db1daa4a3b6f6c3db79e1fb8
Pawel Kacela <pawel.kacela@gmail.com>
2026-03-27 16:51:48 +0100

    add, view for onboarding

--- Features/Onboarding/Sources/OnboardingView.swift
+++ Features/Onboarding/Sources/OnboardingView.swift
@@ -5,4 +5,39 @@
 //  Created by Pawel Kacela on 27/03/2026.
 //
 
+import ComposableArchitecture
 import Foundation
+import SwiftUI
+
+public struct OnboardingView: View {
+
+    let store: StoreOf<OnboardingReducer>
+    
+    public init(store: StoreOf<OnboardingReducer>) {
+        self.store = store
+    }
+
+    public var body: some View {
+
+        VStack {
+            ForEach(store.onboardingPages) { page in
+                Text("title \(page.title)")
+            }
+        }
+
+        Text("Hello, World!")
+    }
+
+}
+
+#Preview {
+    OnboardingView(
+        store:
+            Store(
+                initialState: OnboardingReducer.State(),
+                reducer: {
+                    OnboardingReducer()
+                }
+            )
+    )
+}
