c65bae4e0600979ba278deb9c2df360f2a5dc24c
Pawel Kacela <pawel.kacela@gmail.com>
2026-03-27 16:49:40 +0100

    add, onBoarding files

---
+++ Features/Onboarding/Sources/OnboardingReducer.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingReducer.swift
+//  Onboarding
+//
+//  Created by Pawel Kacela on 27/03/2026.
+//
+
+import Foundation
---
+++ Features/Onboarding/Sources/OnboardingView.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingView.swift
+//  Onboarding
+//
+//  Created by Pawel Kacela on 27/03/2026.
+//
+
+import Foundation
