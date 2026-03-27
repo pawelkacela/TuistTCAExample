3af0c4f74a95516782f4161986fcbda645a13a3e
Pawel Kacela <pawel.kacela@gmail.com>
2026-03-27 16:52:11 +0100

    add, reducer for onboarding

--- Features/Onboarding/Sources/OnboardingReducer.swift
+++ Features/Onboarding/Sources/OnboardingReducer.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+public struct OnboardingReducer {
+    
+    public init() { }
+    
+    @ObservableState
+    public struct State: Equatable {
+        var onboardingPages: [PageDTO]
+        
+        public init(onboardingPages: [PageDTO] = []) {
+            self.onboardingPages = onboardingPages
+        }
+    }
+    
+    public enum Action: Equatable {
+        case nextButtonTapped
+    }
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                case .nextButtonTapped:
+                    print("next tapped")
+                return .none
+            }
+        }
+    }
+    
+}
